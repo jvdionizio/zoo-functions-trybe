5f8f44a3e1391b6117f7fa3b65f455c6cbc059e4
jvdionizio <dionizio145@gmail.com>
2022-01-25 16:39:40 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
+  if (ids == null) {
+    return [];
+  }
 }
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
